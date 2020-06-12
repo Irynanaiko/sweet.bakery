c2c538fb51d4e40811b831be7f64193b9556478e
Irynanaiko <irynaiko@ukr.net>
2020-06-12 09:15:00 +0000

    make some changes

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
+
+const currentDate = new Date().getFullYear();
+
+const saleTime = new Date(`June 18 ${currentDate} 10:00:00`);
+
+// Update countdown time
+function updateCountdown() {
+  const currentTime = new Date();
+  const diff = saleTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  // Add values to DOM
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
+}
+
+// Run every second
+setInterval(updateCountdown, 1000);
